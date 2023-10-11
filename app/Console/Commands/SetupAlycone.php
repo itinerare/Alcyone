7e9fed87458cea9c9aa390210c2a8012561fe49c
itinerare <ad@itinerare.net>
2023-10-11 11:23:10 -0400

    feat(commands): add setup command, site pages command

---
+++ app/Console/Commands/SetupAlycone.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class SetupAlycone extends Command {
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'setup-alcyone';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Runs set-up commands, excluding migrations as first-time migration must be done separately..';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle() {
+        //
+        $this->info('******************');
+        $this->info('* SET UP ALCYONE *');
+        $this->info('******************'."\n");
+
+        // Check if the user has run composer and run migrations
+        $this->info('This command should be run after installing packages using composer and running first-time migrations. Once initial set-up has been performed, you will be prompted for details to set up the site\'s admin account.');
+        if ($this->confirm('Have you run the composer install command or equivalent and run first-time migrations?')) {
+            // Run setup commands
+            $this->line("\n".'Adding site pages...');
+            $this->call('add-site-pages');
+
+            // Run admin user setup
+            $this->line("\n".'Setting up admin user...');
+            $this->call('setup-admin-user');
+        } else {
+            $this->line('Aborting! Please run composer install and php artisan migrate and then run this command again.');
+        }
+    }
+}

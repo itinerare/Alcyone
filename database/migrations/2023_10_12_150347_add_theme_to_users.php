fde494b2032d8400165b045c2ea6b62198231888
itinerare <ad@itinerare.net>
2023-10-13 15:07:28 -0400

    refactor: fix PHP styling

--- database/migrations/2023_10_12_150347_add_theme_to_users.php
+++ database/migrations/2023_10_12_150347_add_theme_to_users.php
@@ -4,13 +4,11 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+    public function up(): void {
         Schema::table('users', function (Blueprint $table) {
             //
             $table->string('theme')->default('dark');
@@ -20,8 +18,7 @@ return new class extends Migration
     /**
      * Reverse the migrations.
      */
-    public function down(): void
-    {
+    public function down(): void {
         Schema::table('users', function (Blueprint $table) {
             //
             $table->dropColumn('theme');

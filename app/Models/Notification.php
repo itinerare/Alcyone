e100a9bfa4ee624011b29427618d92f239938375
itinerare <ad@itinerare.net>
2023-10-17 09:47:02 -0400

    refactor: update relations to class references

--- app/Models/Notification.php
+++ app/Models/Notification.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\User\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Notification extends Model {
@@ -40,7 +41,7 @@ class Notification extends Model {
      * Get the user who owns notification.
      */
     public function user() {
-        return $this->belongsTo('App\Models\User\User');
+        return $this->belongsTo(User::class);
     }
 
     /**********************************************************************************************

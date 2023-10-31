57967f770f950ca26675e11a52815b92e8fc09c0
itinerare <ad@itinerare.net>
2023-10-31 13:01:12 -0400

    feat(tests): basic test setup, access tests - set up user factory

---
+++ tests/Feature/AccessTest.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User\Rank;
+use App\Models\User\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class AccessTest extends TestCase {
+    use RefreshDatabase;
+
+    /******************************************************************************
+        ACCESS/MIDDLEWARE
+    *******************************************************************************/
+
+    protected function setUp(): void {
+        parent::setUp();
+    }
+
+    /**
+     * Test getting the main page.
+     *
+     * @dataProvider accessProvider
+     *
+     * @param bool $user
+     * @param int  $status
+     */
+    public function testGetIndex($user, $status) {
+        if ($user) {
+            $response = $this->actingAs($this->user)->get('/');
+        } else {
+            $response = $this->get('/');
+        }
+
+        $response->assertStatus($status);
+    }
+
+    static function accessProvider() {
+        return [
+            'visitor' => [0, 302],
+            'user'    => [1, 200],
+        ];
+    }
+
+    /**
+     * Test access to account settings.
+     * This should be representative of all member routes.
+     *
+     * @dataProvider memberAccessProvider
+     *
+     * @param bool $user
+     * @param int  $rank
+     * @param int  $status
+     */
+    public function testMemberRouteAccess($user, $rank, $status) {
+        if ($user) {
+            $user = User::factory()->make([
+                'rank_id' => Rank::where('sort', $rank)->first()->id,
+            ]);
+            $response = $this->actingAs($user)->get('/account/settings');
+        } else {
+            $response = $this->get('/account/settings');
+        }
+
+        $response->assertStatus($status);
+    }
+
+    static function memberAccessProvider() {
+        return [
+            'visitor' => [0, 0, 302],
+            'user'    => [1, 0, 200],
+            'editor'  => [1, 1, 200],
+            'admin'   => [1, 2, 200],
+        ];
+    }
+
+    /**
+     * Test access to the admin dashboard.
+     * This should be representative of all editor routes.
+     *
+     * @dataProvider editorAccessProvider
+     *
+     * @param bool $user
+     * @param int  $rank
+     * @param int  $status
+     */
+    public function testEditorRouteAccess($user, $rank, $status) {
+        if ($user) {
+            $user = User::factory()->make([
+                'rank_id' => Rank::where('sort', $rank)->first()->id,
+            ]);
+            $response = $this->actingAs($user)->get('/admin');
+        } else {
+            $response = $this->get('/admin');
+        }
+
+        $response->assertStatus($status);
+    }
+
+    static function editorAccessProvider() {
+        return [
+            'visitor' => [0, 0, 302],
+            'user'    => [1, 0, 302],
+            'editor'  => [1, 1, 200],
+            'admin'   => [1, 2, 200],
+        ];
+    }
+
+    /**
+     * Test access to the admin user index.
+     * This should be representative of all admin-only routes.
+     *
+     * @dataProvider adminAccessProvider
+     *
+     * @param bool $user
+     * @param int  $rank
+     * @param int  $status
+     */
+    public function testAdminRouteAccess($user, $rank, $status) {
+        if ($user) {
+            $user = User::factory()->make([
+                'rank_id' => Rank::where('sort', $rank)->first()->id,
+            ]);
+            $response = $this->actingAs($user)->get('/admin/users');
+        } else {
+            $response = $this->get('/admin/users');
+        }
+
+        $response->assertStatus($status);
+    }
+
+    static function adminAccessProvider() {
+        return [
+            'visitor' => [0, 0, 302],
+            'user'    => [1, 0, 302],
+            'editor'  => [1, 1, 302],
+            'admin'   => [1, 2, 200],
+        ];
+    }
+}

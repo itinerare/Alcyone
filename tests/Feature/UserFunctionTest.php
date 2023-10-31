e5058de1fdb8c58433e76132de7c20213d3c0086
itinerare <ad@itinerare.net>
2023-10-31 14:47:54 -0400

    feat(tests): add user function tests

---
+++ tests/Feature/UserFunctionTest.php
@@ -0,0 +1,121 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\User\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Hash;
+use Tests\TestCase;
+
+class UserFunctionTest extends TestCase {
+    use RefreshDatabase, WithFaker;
+
+    /******************************************************************************
+        USER / SETTINGS
+    *******************************************************************************/
+
+    protected function setUp(): void {
+        parent::setUp();
+
+        $this->user = User::factory()->create();
+    }
+
+    /**
+     * Test theme editing.
+     *
+     * @dataProvider userEditProvider
+     *
+     * @param bool $isValid
+     * @param bool $expected
+     */
+    public function testPostEditTheme($isValid, $expected) {
+        // Generate some test data
+        if ($isValid) {
+            $theme = 'light';
+        } else {
+            $theme = $this->faker->domainWord();
+        }
+
+        $response = $this->actingAs($this->user)
+            ->post('account/theme', [
+                'theme' => $theme,
+            ]);
+
+        if ($expected) {
+            $response->assertSessionHasNoErrors();
+            $this->assertDatabaseHas('users', [
+                'id'    => $this->user->id,
+                'theme' => $theme,
+            ]);
+        } else {
+            $response->assertSessionHasErrors();
+        }
+    }
+
+    /**
+     * Test email editing.
+     *
+     * @dataProvider userEditProvider
+     *
+     * @param bool $isValid
+     * @param bool $expected
+     */
+    public function testPostEditEmail($isValid, $expected) {
+        // Generate some test data
+        if ($isValid) {
+            $email = $this->faker->unique()->safeEmail();
+        } else {
+            $email = $this->faker->domainWord();
+        }
+
+        $response = $this->actingAs($this->user)
+            ->post('account/email', [
+                'email' => $email,
+            ]);
+
+        if ($expected) {
+            $response->assertSessionHasNoErrors();
+            $this->assertDatabaseHas('users', [
+                'id'    => $this->user->id,
+                'email' => $email,
+            ]);
+        } else {
+            $response->assertSessionHasErrors();
+        }
+    }
+
+    /**
+     * Test password editing.
+     *
+     * @dataProvider userEditProvider
+     *
+     * @param bool $isValid
+     * @param bool $expected
+     */
+    public function testPostEditPassword($isValid, $expected) {
+        // Make a persistent user with a simple password
+        $user = User::factory()->simplePass()->create();
+
+        $response = $this->actingAs($user)
+            ->post('account/password', [
+                'old_password'              => 'simple_password',
+                'new_password'              => 'password',
+                'new_password_confirmation' => $isValid ? 'password' : 'not_password',
+            ]);
+
+        if ($expected) {
+            $response->assertSessionHasNoErrors();
+            $this->assertTrue(Hash::check('password', $user->fresh()->password));
+        } else {
+            $response->assertSessionHasErrors();
+        }
+    }
+
+    public static function userEditProvider() {
+        return [
+            'valid'   => [1, 1],
+            'invalid' => [0, 0],
+        ];
+    }
+}
